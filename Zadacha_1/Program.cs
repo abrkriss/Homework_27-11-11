7125f8df4847fab3e5a0af419fddeb7526780161
Kristina <abrkriss@gmail.com>
2022-12-03 16:19:04 +0300

    Zadacha 1 + 2

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,65 @@
+﻿void Zadacha41()
+{
+    //Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+    Console.Write("Введите числа через запятую  ");
+    int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+   int[] numbers = StringToNum(Console.ReadLine());
+    PrintArray(numbers);
+    int counter = 0;
+    for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        counter++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {counter}");
+
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+Zadacha41();
